7b0360d7277baa89711845ffcd387f855a59475d
elifyildirim <eliiifyldrm96@gmail.com>
2019-04-25 23:41:46 +0300

    Creating Todos,Updating Todos,Deleting Todos,Logout and Login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,8 +1,73 @@
-import React from 'react';
+// import React from 'react';
+// import './styles.css';
+// const Login = () => (
+//   <div className="App">
+//     <h1>Login</h1>
+//   </div>
+// );
+// export default Login;
+
+import React, { useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import './styles.css';
-const Login = () => (
-  <div className="App">
-    <h1>Login</h1>
-  </div>
-);
-export default Login;
+const Login = (props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleOnChange = (event, key) => {
+    if (key === 'email') {
+      setEmail(event.currentTarget.value);
+    } else if (key === 'password') {
+      setPassword(event.currentTarget.value);
+    }
+  }
+  const handleOnSubmit = (event) => {
+    // Reset our error every time the form is submitted
+    setError(null);
+    setLoading(true);
+    fetch(
+      'http://localhost:5000/api/auth/login',
+      {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password
+        })
+    })
+    .then((response) => {
+      const json = response.json();
+      if (response.ok) {
+        return json;
+      }
+      
+      return json.then((data) => Promise.reject(data));
+    })
+    .then((json) => {
+      setLoading(false);
+      localStorage.setItem('token', json.token);
+      props.history.push('/todos');
+    })
+    .catch((error) => {
+      setLoading(false);
+      setError(error.error);
+    });
+    event.preventDefault(); // to prevent the form from submitting
+  }
+  return (
+    <div className="App">
+      <h1>Login</h1>
+      <form onSubmit={handleOnSubmit}>
+        <input disabled={loading} type="email" onChange={(event) => handleOnChange(event, 'email')} required="required" value={email} placeholder="Email address" />
+        <input disabled={loading} type="password" onChange={(event) => handleOnChange(event, 'password')} required="required" value={password} placeholder="Password" />
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        <button disabled={loading} type="submit">Submit</button>
+      </form>
+      <Link to="/register">Register</Link>
+    </div>
+  );
+};
+export default withRouter(Login);
--- src/components/Todos/index.js
+++ src/components/Todos/index.js
@@ -1,43 +1,201 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
 import './styles.css';
-const Todos = () => {
-  const [todos, setTodos] = useState([{
-    id: '1234',
-    text: 'Hello',
-    done: false
-  }]);
+const Todos = (props) => {
+//   const [todos, setTodos] = useState([{
+//     id: '1234',
+//     text: 'Hello',
+//     done: false
+//   }]);
+  const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('New Todo');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    if (!localStorage.token) {
+        props.history.push('/login');
+      } else {
+        getTodos();
+      }
+    console.log('COMPONENT LOADED');
+    getTodos();
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    props.history.push('/login');
+  }
+
+  const getTodos = () => {
+    setLoading(true);
+    setError(null); 
+    fetch(
+      'http://localhost:5000/api/todos',
+      {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': localStorage.token
+        }
+      }
+    )
+    .then((response) => {
+      const json = response.json();
+      if (response.ok) {
+        return json;
+      }
+      return json.then((data) => Promise.reject(data));
+    })
+    .then((json) => {
+      console.log('SUCCESS');
+      console.log(json);
+      setTodos(json.todos);
+      setLoading(false);
+    })
+    .catch((error) => {
+      setLoading(false);
+      setError(error.error);
+    });
+  }
+
+  const createTodo = (data) => {
+    setLoading(true);
+    setError(null);
+    fetch(
+      'http://localhost:5000/api/todos',
+      {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': localStorage.token
+        },
+        body: JSON.stringify(data)
+      }
+    )
+    .then((response) => {
+      const json = response.json();
+      if (response.ok) {
+        return json;
+      }
+      return json.then((data) => Promise.reject(data));
+    })
+    .then(() => {
+      setNewTodo(''); // reset input field
+      getTodos(); // retrieve latest todos
+    })
+    .catch((error) => {
+      setLoading(false);
+      setError(error.error);
+    });
+  }
+
+  const updateTodo = (id, data) => {
+    setLoading(true);
+    setError(null);
+    fetch(
+      `http://localhost:5000/api/todos/${id}`,
+      {
+        method: 'put',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': localStorage.token
+        },
+        body: JSON.stringify(data)
+      }
+    )
+    .then((response) => {
+      const json = response.json();
+      if (response.ok) {
+        return json;
+      }
+      return json.then((data) => Promise.reject(data));
+    })
+    .then(() => {
+      getTodos();
+    })
+    .catch((error) => {
+      setLoading(false);
+      setError(error.error);
+    });
+  }
+
+  const deleteTodo = (id) => {
+    setLoading(true);
+    setError(null);
+    fetch(
+      `http://localhost:5000/api/todos/${id}`,
+      {
+        method: 'delete',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': localStorage.token
+        }
+      }
+    )
+    .then((response) => {
+      const json = response.json();
+      if (response.ok) {
+        return json;
+      }
+  return json.then((data) => Promise.reject(data));
+    })
+    .then(() => {
+      getTodos();
+    })
+    .catch((error) => {
+      setLoading(false);
+      setError(error.error);
+    });
+  }
+
   const handleOnChange = (event) => {
     setNewTodo(event.currentTarget.value);
   }
   const handleOnKeyPress = (event) => {
+    // createTodo({
+    //     text: event.currentTarget.value,
+    //     done: false
+    //   });
     if (event.key === 'Enter') {
-      const newTodos = todos;
-      newTodos.push({
-        id: Math.floor(Math.random() * (10000-1000+1) + 1000),
-        text: event.currentTarget.value,
-        done: false,
-      });
+        createTodo({
+            text: event.currentTarget.value,
+            done: false
+          });
+    //   const newTodos = todos;
+    //   newTodos.push({
+    //     id: Math.floor(Math.random() * (10000-1000+1) + 1000),
+    //     text: event.currentTarget.value,
+    //     done: false,
+    //   });
       
-      setTodos(newTodos);
-      setNewTodo('');
+    //   setTodos(newTodos);
+    //   setNewTodo('');
     }
   }
   const handleDeleteTodo = (index) => {
-    const newTodos = [ ...todos.slice(0, index), ...todos.slice(index + 1) ];
-    setTodos(newTodos);
+    deleteTodo(todos[index].id);
+    // const newTodos = [ ...todos.slice(0, index), ...todos.slice(index + 1) ];
+    // setTodos(newTodos);
   }
   const handleUpdateTodo = (index) => {
     const newTodos = [ ...todos ];
-    newTodos[index].done = !newTodos[index].done;
-    setTodos(newTodos);
+    const data = {
+        text: newTodos[index].text, 
+        done: !newTodos[index].done
+      };
+    updateTodo(newTodos[index].id, data);
+    // newTodos[index].done = !newTodos[index].done;
+    // setTodos(newTodos);
   }
   return (
     <div className="App">
       <h1>Todos</h1>
-      <input type="text" value={newTodo} placeholder="Enter todo here" onChange={handleOnChange} onKeyPress={handleOnKeyPress}/>
+      <button type="button" onClick={handleLogout}>Logout</button>
+      <input disabled={loading} type="text" value={newTodo} placeholder="Enter todo here" onChange={handleOnChange} onKeyPress={handleOnKeyPress}/>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
       <ul>
-        {(todos).map((val, key) => 
+        {todos && todos.length === 0 && <li>No Todos Yet...</li>}
+        {todos && todos.length > 0 && (todos).map((val, key) => 
           <li key={val.id}>
             {val.text} - {val.done ? (<span onClick={() => handleUpdateTodo(key)} role="img" aria-label="">✅</span>) : (<span onClick={() => handleUpdateTodo(key)} role="img" aria-label="">⏹️</span>)}
             <button onClick={() => handleDeleteTodo(key)}><span role="img" aria-label="">❌</span></button>
@@ -46,4 +204,4 @@ const Todos = () => {
     </div>
   )
 };
-export default Todos;
+export default withRouter(Todos);
